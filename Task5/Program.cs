640ecd242f22d62e8e3daf77ecc8d8323b115d7a
Olga <workpost.mkh@yandex.ru>
2023-09-06 15:47:22 +0300

    Task 5

---
+++ Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿// напишите программу которая будет создавать копию заданого массива с помощью поэлементного копирования
+
+int[] numbers = {1, 2, 3, 4, 5, 6, 7};
+int[] copy = new int[numbers.Length];
+for (int i = 0; i < numbers.Length; i++)
+{
+    copy[i] = numbers[i];
+}
+Console.WriteLine(string.Join(", ", copy));
+numbers[3] = 10; // присвоим 3-му элементу 10, чтобы показать что копия не изменилась
+Console.WriteLine(string.Join(", ", copy));
